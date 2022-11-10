56267df18cacaca3edc3e1e0e785f6ba6be66805
dulekt <dulekt@gmail.com>
2022-11-10 13:30:24 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import Login from './components/Login';
+import PickLabel from './components/PickLabel';
 
 function App() {
+  const [isLoggedin, setIsLoggedin] = useState(false);
+  const [user, setUser] = useState("")
+  const [orderNumber, setOrderNumber] = useState()
+  const [label, setLabel] = useState()
+  const [printLabel, setPrintLabel] = useState(false)
+  const [content, setContent] = useState({ "": 0 })
+
+
+  const handleLogin = () => {
+    setIsLoggedin(true);
+  }
+  const handleLogout = () => {
+    setIsLoggedin(false);
+  }
+  const handleUser = (user) => {
+    setUser(user);
+  }
+  const assignOrder = (value) => {
+    setOrderNumber(value);
+  }
+  const assignLabel = (value) => {
+    setLabel(value);
+  }
+  if (!isLoggedin) {
+    return (
+      <div className='container'>
+        <ChakraProvider >
+          <div className="App"  >
+            <h1 className='Header'>System zamawiania oznaczen <br /></h1>
+            <Login
+              isLoggedin={isLoggedin}
+              handleLogin={handleLogin}
+              user={user}
+              handleUser={handleUser}
+              orderNumber = {orderNumber}
+              assignOrder={assignOrder}
+
+
+            />
+          </div>
+        </ChakraProvider>
+
+      </div>
+    );
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <ChakraProvider >
+
+        <div className="App"  >
+          <h1 className='Header'>System zamawiania oznaczen <br /></h1>
+          <PickLabel
+            handleLogout={handleLogout}
+            assignLabel={assignLabel} />
+
+        </div>
+      </ChakraProvider>
+
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Header() {
+  return (
+
+
+    <div class="Header" >
+
+
+      <h1>System Zamawiania Etykiet </h1>
+    </div>
+  );
+  }
+
+  export default Header;
