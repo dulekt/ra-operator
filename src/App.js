1c1f6fdf0b7e36cc0101380a659b49cb79e1a38a
dulekt <dulekt@gmail.com>
2022-11-15 15:38:18 +0100

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,78 +1,32 @@
-import { useState } from 'react';
+//App.js
 import './App.css';
-import LogInView from './views/LogInView';
-import LoginButton from './components/LoginButton';
-import SetLabel from './components/setLabelButton';
-import { ChakraProvider, Stack } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Container, ChakraProvider } from '@chakra-ui/react';
+import AppHeader from './components/AppHeader';
+import AppContent from './components/AppContent';
+import AppFooter from './components/AppFooter';
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [order, setOrder] = useState({
-    user: "",
-    orderNumber: "",
-    labelType: "",
-    content: ""
-  })
   function handleLogin() {
-    setIsLoggedIn(!isLoggedIn)
-
+    setIsLoggedIn(true)
   }
-  function changeOrderNumber(e) {
-    setOrder({
-      ...order,
-      orderNumber: e.target.value
-    })
+  function handleLogout() {
+    setIsLoggedIn(false)
   }
-  function changePrintLabel(label) {
-    setOrder({
-      ...order,
-      printLabel: { label }
-    })
-  }
-
-  function changeUser(e) {
-    setOrder({
-      ...order,
-      user: e.target.value
-    })
-  }
-
-  if (!isLoggedIn) {
-    return (
-      <ChakraProvider>
-        <p>Username: {order.user}</p>
-        <p>ON: {order.orderNumber}</p>
-        <p>Label: {order.labelType}</p>
-        <p>{order.content}</p>
-        <></>
-        <Stack>
-          <div className='container'>
-            <LogInView
-              order={order}
-              changeOrderNumber={changeOrderNumber}
-              changeUser={changeUser}
-            />
-          </div>
-          <LoginButton
-            isLoggedIn={isLoggedIn}
-            handleLogin={handleLogin} />
-        </Stack>
-
-
-      </ChakraProvider>
-    );
-  }
-
+  
   return (
     <ChakraProvider>
-      <Stack>
-        <SetLabel
-          changePrintLabel={changePrintLabel} />
-        <LoginButton
+      <Container centerContent>
+        <AppHeader />
+        <AppContent
           isLoggedIn={isLoggedIn}
-          handleLogin={handleLogin} />
-      </Stack>
-
-    </ChakraProvider>
-  )
+        />
+        <AppFooter
+          isLoggedIn={isLoggedIn}
+          handleLogin={handleLogin}
+          handleLogout={handleLogout}
+        />
+      </Container>
+    </ChakraProvider>)
 }
