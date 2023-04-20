af41d506e5aa995fc4c5096fb79422df7de2e69d
dulekt <dulekt@gmail.com>
2023-04-20 08:44:53 +0200

    label image added as modal

--- src/components/ModalLabels.jsx
+++ src/components/ModalLabels.jsx
@@ -10,22 +10,25 @@ import {
     useDisclosure,
 } from '@chakra-ui/react';
 
+import Etykietyv1 from '../assets/images/Etykietyv1.png';
+
 export default function ModalLabels() {
     const { isOpen, onOpen, onClose } = useDisclosure();
 
     return (
         <>
-            {/*
-                <Button colorScheme="blue" size="sm" onClick={onOpen} variant="outline" m={1}>
+            {
+                <Button colorScheme="red" size="sm" onClick={onOpen} variant="outline" m={1}>
                     Pokaz naklejki
-                </Button> */}
+                </Button>
+            }
 
             <Modal isOpen={isOpen} onClose={onClose} size="xl">
                 <ModalOverlay bg="none" backdropFilter="auto" backdropInvert="30%" backdropBlur="5px" />
                 <ModalContent>
                     <ModalCloseButton />
                     <ModalBody>
-                        <Image src="https://i.imgur.com/j9LqB9C.png" alt="logo" />
+                        <Image src={Etykietyv1} alt="naklejki" />
                     </ModalBody>
                     <ModalFooter>
                         <Button colorScheme="blue" mr={3} size="sm" onClick={onClose}>
